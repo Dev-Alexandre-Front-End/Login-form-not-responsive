10bcbacf426221cc7b181f1f6a1bddb793a91706
Dev-Alexandre-Front-End <alexvice0000@gmail.com>
2025-08-22 16:13:10 -0400

    Criar main.js

---
+++ main.js
@@ -0,0 +1,84 @@
+const icon = document.querySelector(".fa-eye");
+const usr_pass = document.querySelector("#usr_passrd");
+const usr_gmail = document.querySelector("#usr_email");
+const usr_check = document.querySelector("#check");
+
+const er_email = document.querySelector("#emal-u");
+const er_password = document.querySelector("#pass-u");
+const er_check = document.querySelector("#chec-u");
+
+const btn = document.querySelector("#usr-submit");
+const form = document.querySelector("form");
+
+const emailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
+
+function iconBTN() {
+  if (usr_pass.type === "password") {
+    usr_pass.type = "text";
+    icon.classList.remove("fa-eye");
+    icon.classList.add("fa-eye-slash");
+  } else {
+    usr_pass.type = "password";
+    icon.classList.add("fa-eye");
+    icon.classList.remove("fa-eye-slash");
+  }
+}
+btn.addEventListener('click', (event) => {
+  event.preventDefault();
+  
+  const pass = usr_pass.value.trim();
+  const gmail = usr_gmail.value.trim();
+  const check = usr_check.checked;
+  
+  let validate = true;
+  
+  er_email.classList.remove("ema");
+  er_password.classList.remove("pas");
+  er_check.classList.remove("che");
+  
+  usr_gmail.classList.remove("error");
+  usr_pass.classList.remove("error");
+  
+  if (pass.length < 6) {
+    er_password.classList.add("pas");
+    usr_pass.classList.add("error");
+    validate = false;
+  }
+  if ((pass.startsWith(" ") || (pass.endsWith(" ")) || (pass.includes(" ")))) {
+    er_password.classList.add("pas");
+    usr_pass.classList.add("error");
+    validate = false;
+  }
+  if (!emailRegex.test(gmail)) {
+    er_email.classList.add("ema");
+    usr_gmail.classList.add("error");
+    validate = false;
+  }
+  
+  if (!check) {
+    er_check.classList.add("che");
+    validate = false;
+  }
+  
+  if (!validate) return;
+  
+  
+  btn.classList.add("sub");
+  btn.innerHTML = "";
+  
+  setTimeout(() => {
+    btn.classList.remove("sub");
+    btn.innerHTML = "ENVIADO";
+    btn.disabled = true;
+    
+    form.reset();
+    
+    
+    er_email.classList.remove("ema");
+    er_password.classList.remove("pas");
+    er_check.classList.remove("che");
+    
+    usr_gmail.classList.remove("error");
+    usr_pass.classList.remove("error");
+  }, 5000);
+});
